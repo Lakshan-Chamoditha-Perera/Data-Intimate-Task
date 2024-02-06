770d87c193c24ee4888c5d99860137f507260a25
Lakshan Chamoditha Perera <perera.alc2000@gmail.com>
2024-02-06 15:41:55 +0530

    jwt verified : done

--- src/util/middleware/VerifyToken.ts
+++ src/util/middleware/VerifyToken.ts
@@ -4,13 +4,13 @@ import process from "process";
 
 const verifyToken = (req: express.Request, res: any, next: express.NextFunction) => {
     const token = req.headers.authorization;
-    if (!token) return res.status(401).json('Invalid token')
+    if (!token) return res.status(401).json('empty token')
     try {
         const data = jwt.verify(token, process.env.JWT_SECRET as Secret);
         res.tokenData = data;
         next();
     } catch (error) {
-        return res.status(401).json('Invalid token')
+        return res.status(401).json('invalid token')
     }
 }
 export default verifyToken;
